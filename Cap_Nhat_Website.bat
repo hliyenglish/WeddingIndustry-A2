c933bca95b29d65db2011e1b850ba026982e9a73
Admin <admin@example.com>
2026-07-20 12:50:01 +0700

    Auto update from desktop

---
+++ Cap_Nhat_Website.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Dang tien hanh dong bo len GitHub...
+"D:\antigravity\MinGit\cmd\git.exe" add .
+"D:\antigravity\MinGit\cmd\git.exe" commit -m "Auto update from desktop"
+"D:\antigravity\MinGit\cmd\git.exe" push origin main
+echo.
+echo Hoan thanh!
+echo Luu y: Trong lan dau tien chay file nay, GitHub se hien len mot cua so yeu cau ban dang nhap. Ban hay bam dang nhap bang trinh duyet nhe!
+echo.
+pause
